85023f32ba32019aaa78ed581fe727daaeeb8ac1
shunkat <shunkat@icloud.com>
2024-12-19 15:05:17 +0900

    google-serviceのライブラリ追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jlleitschuh.gradle.ktlint")
+    id("com.google.gms.google-services")
 }
 
 android {
