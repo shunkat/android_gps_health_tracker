70bd3a1d56e9f08ffada1c5cd54034d75be4372e
shunkat <shunkat@icloud.com>
2024-12-21 12:15:47 +0900

    composeのナビゲーションを実装

--- app/src/main/java/com/shunk0616/gps_health_connect/ui/GpsHealthConnectApp.kt
+++ app/src/main/java/com/shunk0616/gps_health_connect/ui/GpsHealthConnectApp.kt
@@ -5,18 +5,35 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.shunk0616.gps_health_connect.data.source.Route
 import com.shunk0616.gpshealthconnect.ui.auth.AuthenticationScreen
 import com.shunk0616.gpshealthconnect.ui.common.theme.gpshealthconnectTheme
+import com.shunk0616.gpshealthconnect.ui.home.HomeScreen
+import com.shunk0616.gpshealthconnect.ui.setting.SettingScreen
 
 @Composable
 fun GpsHealthConnectApp() {
+    val navController = rememberNavController()
+
     gpshealthconnectTheme {
-        // A surface container using the 'background' color from the theme
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            AuthenticationScreen()
+            NavHost(
+                navController = navController,
+                startDestination = Route.Home
+            ) {
+                composable<Route.Home> { HomeScreen() }
+                composable<Route.Authenticatioin> { AuthenticationScreen() }
+                composable<Route.Setting> { SettingScreen() }
+            }
         }
     }
+
+
 }
+
