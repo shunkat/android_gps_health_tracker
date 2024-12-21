f3b8a59fe2fea9c433465fa9bb2a1b1c5896a1ad
shunkat <shunkat@icloud.com>
2024-12-21 11:45:11 +0900

    serializeのプラグインを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("org.jlleitschuh.gradle.ktlint")
     id("com.google.gms.google-services")
+    kotlin("plugin.serialization")
 }
 
 android {
@@ -61,6 +62,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.navigation:navigation-compose:2.8.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+
 
     // firebase
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,6 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    kotlin("jvm") version "1.9.0" apply false
+    kotlin("plugin.serialization") version "1.9.0" apply false
 }
