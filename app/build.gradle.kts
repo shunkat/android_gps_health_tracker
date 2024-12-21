7c211a55d4c0553bc20252c8436cfd721a592b09
shunkat <shunkat@icloud.com>
2024-12-21 12:03:16 +0900

    serializationプラグインのバージョンをkotlinに合わせてダウングレード

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.8.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
 
     // firebase
