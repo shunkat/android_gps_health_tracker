f771c2c354308be5647b4d0fa50b2670c626fe52
shunkat <shunkat@icloud.com>
2024-12-21 12:03:30 +0900

    Routeオブジェクトを定義

---
+++ app/src/main/java/com/shunk0616/gps_health_connect/data/source/Route.kt
@@ -0,0 +1,14 @@
+package com.shunk0616.gps_health_connect.data.source
+
+import kotlinx.serialization.Serializable
+
+class Route {
+    @Serializable
+    data object Authenticatioin
+
+    @Serializable
+    data object Setting
+
+    @Serializable
+    data object Home
+}
