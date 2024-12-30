fbc22c2d942571389609737ccb0cb693c6b49ea1
shunkat <shunkat@icloud.com>
2024-12-30 12:47:55 +0900

    Navigationの大元を定義するファイル名の変更と実装

---
+++ app/src/main/java/com/shunk0616/gpshealthconnect/navigation/GhcNavHost.kt
@@ -0,0 +1,31 @@
+package com.shunk0616.gpshealthconnect.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.Home
+import com.shunk0616.gpshealthconnect.ui.GhcAppState
+import com.shunk0616.gpshealthconnect.ui.home.navigation.HomeRoute
+import com.shunk0616.gpshealthconnect.ui.home.navigation.homeScreen
+import com.shunk0616.gpshealthconnect.ui.setting.navigation.navigateToSetting
+import com.shunk0616.gpshealthconnect.ui.setting.navigation.settingScreen
+
+@Composable
+fun GhcNavHost(
+    appState: GhcAppState,
+    modifier: Modifier = Modifier,
+) {
+    val navController = appState.navController
+    NavHost(
+        navController = navController,
+        startDestination = HomeRoute,
+        modifier = modifier
+    ) {
+        homeScreen(
+            onSettingClick = { navController.navigateToSetting() }
+        )
+        settingScreen(
+            onBackClick = { navController.popBackStack() }
+        )
+    }
+}
