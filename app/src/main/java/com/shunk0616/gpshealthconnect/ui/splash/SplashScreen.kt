1d95092db1f58ef022d69e698812a16a4a52eba7
shunkat <shunkat@icloud.com>
2025-01-02 16:27:52 +0900

    ktlintのフォーマットをかけた

--- app/src/main/java/com/shunk0616/gpshealthconnect/ui/splash/SplashScreen.kt
+++ app/src/main/java/com/shunk0616/gpshealthconnect/ui/splash/SplashScreen.kt
@@ -1,21 +1,16 @@
 package com.shunk0616.gpshealthconnect.ui.splash
 
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import kotlinx.coroutines.delay
 
 private const val SPLASH_TIMEOUT = 1000L
 
 @Composable
-internal fun SplashRoute(
-) {
+internal fun SplashRoute() {
     SplashScreen()
 }
 
 @Composable
 fun SplashScreen() {
-        CircularProgressIndicator()
+    CircularProgressIndicator()
 }
