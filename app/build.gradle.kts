ce97bae0ae8c65b7e88b0bddecda0e89e9106a16
shunkat <shunkat@icloud.com>
2025-01-03 16:37:19 +0900

    hilt導入に先立って、KSP導入とkotlinのバージョン上げ - kotlin自体のバージョンを2.1.0に - 上記に伴ってcompilerのプラグインが必要になったので導入 - kspの導入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,10 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
     id("org.jlleitschuh.gradle.ktlint")
     id("com.google.gms.google-services")
+    id("com.google.devtools.ksp")
     kotlin("plugin.serialization")
 }
 
@@ -34,11 +36,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
