af282cb0931ff60679d1153068ca727a92f23bce
shunkat <shunkat@icloud.com>
2025-01-05 15:35:02 +0900

    この後必要になるライブラリのインポート

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.2" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     kotlin("jvm") version "1.9.0" apply false
