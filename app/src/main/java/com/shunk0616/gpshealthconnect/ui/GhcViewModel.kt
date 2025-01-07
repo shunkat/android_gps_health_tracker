e87b12a9595b4d0380790dbe150c82a698354b1f
shunkat <shunkat@icloud.com>
2025-01-07 12:23:17 +0900

    ベースとなるviewmodelを実装

---
+++ app/src/main/java/com/shunk0616/gpshealthconnect/ui/GhcViewModel.kt
@@ -0,0 +1,20 @@
+package com.shunk0616.gpshealthconnect.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.shunk0616.gpshealthconnect.data.repository.LogRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+open class GhcViewModel(
+    private val logRepository: LogRepository,
+): ViewModel() {
+    fun launchCatching(snackbar: Boolean = true, block: suspend CoroutineScope.() -> Unit) =
+        viewModelScope.launch(
+            CoroutineExceptionHandler { _, throwable ->
+                logRepository.logNonFatalCrash(throwable)
+            },
+            block = block
+    )
+}
