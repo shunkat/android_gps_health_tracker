348c83888de024ff81f7026aaf27745138fc3a54
shunkat <shunkat@icloud.com>
2025-01-07 14:12:41 +0900

    フォーマット後のコミット漏れ

--- app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/impl/LogRepositoryImpl.kt
+++ app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/impl/LogRepositoryImpl.kt
@@ -5,7 +5,7 @@ import com.google.firebase.crashlytics.crashlytics
 import com.shunk0616.gpshealthconnect.data.repository.LogRepository
 import javax.inject.Inject
 
-class LogRepositoryImpl @Inject constructor(): LogRepository {
+class LogRepositoryImpl @Inject constructor() : LogRepository {
     override fun logNonFatalCrash(thromwable: Throwable) {
         Firebase.crashlytics.recordException(thromwable)
     }
