480768f14657c6ef60b3f0aaa4b9d80b7192d485
shunkat <shunkat@icloud.com>
2025-01-07 12:34:18 +0900

    フォーマットをかける

--- app/src/main/java/com/shunk0616/gpshealthconnect/ui/GhcViewModel.kt
+++ app/src/main/java/com/shunk0616/gpshealthconnect/ui/GhcViewModel.kt
@@ -8,13 +8,13 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 open class GhcViewModel(
-    private val logRepository: LogRepository,
-): ViewModel() {
+    private val logRepository: LogRepository
+) : ViewModel() {
     fun launchCatching(snackbar: Boolean = true, block: suspend CoroutineScope.() -> Unit) =
         viewModelScope.launch(
             CoroutineExceptionHandler { _, throwable ->
                 logRepository.logNonFatalCrash(throwable)
             },
             block = block
-    )
+        )
 }
