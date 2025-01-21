4f7403296f64d40a08dd1f0fdc3a98db3e6a4449
shunkat <shunkat@icloud.com>
2025-01-21 18:21:07 +0900

    AccountRepositorにlogin関数を実装

--- app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/AccountRepository.kt
+++ app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/AccountRepository.kt
@@ -3,7 +3,5 @@ package com.shunk0616.gpshealthconnect.data.repository
 interface AccountRepository {
     val isAuthorized: Boolean
 
-//    suspend fun login(email: String)
-//    suspend fun logout()
-//    suspend fun deleteAccount()
+    suspend fun login()
 }
--- app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/impl/AccountRepositoryImpl.kt
+++ app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/impl/AccountRepositoryImpl.kt
@@ -1,11 +1,19 @@
 package com.shunk0616.gpshealthconnect.data.repository.impl
 
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 import com.shunk0616.gpshealthconnect.data.repository.AccountRepository
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 class AccountRepositoryImpl @Inject constructor(
-//    private val auth: FirebaseAuth
+    private val auth: FirebaseAuth
 ) : AccountRepository {
     override val isAuthorized: Boolean
         get() = false
+
+    override suspend fun login() {
+        Log.d("AccountRepositoryImpl", "login: $email, $password")
+        auth.signInAnonymously().await()
+    }
 }
