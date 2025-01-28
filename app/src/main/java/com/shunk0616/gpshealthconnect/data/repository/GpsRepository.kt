093d063dd88720db2ae265312911a0c0c90add90
shunkat <shunkat@icloud.com>
2025-01-28 18:01:24 +0900

    位置情報取得用のレポジトリのクラスを一時的な取得から継続取得に変更（一時取得の関数を削除し、開始と停止の関数を追加）

--- app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/GpsRepository.kt
+++ app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/GpsRepository.kt
@@ -1,27 +1,42 @@
 package com.shunk0616.gpshealthconnect.data.repository
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.location.Location
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.shunk0616.gpshealthconnect.GhcActivity
 
 class GpsRepository(private val context: Context) {
+    private var getRate: Long = 10000
+    private var minRate: Long = 5000
+
     private var fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(context)
+    private var locationRequest: LocationRequest? = null
+    private var locationCallback: LocationCallback? = null
 
     interface MyLocationCallback {
         fun onLocationResult(location: Location?)
         fun onLocationError(error: String)
     }
 
-    fun getLastLocation(callback: MyLocationCallback) {
-        // 最初に権限をチェック
-        if(ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != 0
-            && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != 0) {
+    init {
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, getRate)
+            .setMinUpdateIntervalMillis(minRate)
+            .build()
+    }
+
+    fun startLocationUpdates(callback: MyLocationCallback) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != 0
+            && ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != 0) {
             if (context is GhcActivity) {
                 ActivityCompat.requestPermissions(
                     context,
@@ -34,22 +49,26 @@ class GpsRepository(private val context: Context) {
             }
             return
         }
-        requestLocation(callback)
-    }
 
-    @SuppressLint("MissingPermission")
-    private fun requestLocation(callback: GpsRepository.MyLocationCallback) {
-        // 位置情報の取得
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                if (location != null) {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
                     callback.onLocationResult(location)
-                } else {
-                    callback.onLocationError("Location is null")
                 }
             }
-            .addOnFailureListener { exception ->
-                callback.onLocationError(exception.message ?: "Unknown error")
+
+            override fun onLocationAvailability(availability: LocationAvailability) {
+                if (!availability.isLocationAvailable) {
+                    callback.onLocationError("Location is not available")
+                }
             }
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest!!, locationCallback!!, null)
+    }
+
+    fun stopLocationUpdates() {
+        locationCallback?.let {
+            fusedLocationClient.removeLocationUpdates(it)
+        }
     }
 }
