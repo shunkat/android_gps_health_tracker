1e17a6ae062659f32bef4a24a4144b2f9343e635
shunkat <shunkat@icloud.com>
2025-02-02 19:17:05 +0900

    gps情報をサーバーに保存するための関数の中身を実装

---
+++ app/src/main/java/com/shunk0616/gpshealthconnect/data/repository/impl/GpsRepositoryImpl.kt
@@ -0,0 +1,20 @@
+package com.shunk0616.gpshealthconnect.data.repository.impl
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.shunk0616.gpshealthconnect.data.repository.GpsRepository
+import javax.inject.Inject
+
+
+class GpsRepositoryImpl @Inject constructor(
+    private val firestore: FirebaseFirestore
+) : GpsRepository {
+    override suspend fun saveLocation(latitude: Double, longitude: Double) {
+        firestore.collection("gps").add(
+            hashMapOf(
+                "latitude" to latitude,
+                "longitude" to longitude,
+                "timestamp" to System.currentTimeMillis()
+            )
+        )
+    }
+}
