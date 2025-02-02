b55bb40884f6c4c284f678c7d54ae8e62ac8a3a4
shunkat <shunkat@icloud.com>
2025-02-02 18:14:01 +0900

    foreground serviceとして位置情報の収集を行うためのクラスの作成とmanifestへの登録

---
+++ app/src/main/java/com/shunk0616/gpshealthconnect/domain/service/GPSForegroundService.kt
@@ -0,0 +1,74 @@
+package com.shunk0616.gpshealthconnect.domain.service
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import com.shunk0616.gpshealthconnect.GhcActivity
+import com.shunk0616.gpshealthconnect.R
+
+class GPSForegroundService : Service() {
+    private lateinit var gpsLocationManager: GPSLocationManager
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        gpsLocationManager = GPSLocationManager(this)
+        gpsLocationManager.startLocationUpdates(object : GPSLocationManager.MyLocationCallback {
+            override fun onLocationResult(location: Location?) {
+                location?.let {
+                    // TODO: Firestoreへの送信処理
+                    Log.d("GPSForegroundService", "Location: ${location.latitude}, ${location.longitude}")
+                }
+            }
+
+            override fun onLocationError(e: Exception) {
+                Log.e("GPSForegroundService", "Location Error: ${e.message}")
+            }
+        })
+        startForegroundServiceWithNotification()
+        return START_STICKY
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun startForegroundServiceWithNotification() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "GPSServiceChannel"
+        val channelName = "GPS Service Channel"
+
+        // 通知チャンネルを作成
+        val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_LOW)
+        notificationManager.createNotificationChannel(channel)
+
+        // メインアクティビティを起動するPendingIntent
+        val notificationIntent = Intent(this, GhcActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+
+        // 通知の作成
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("GPS 位置情報サービス")
+            .setContentText("位置情報を取得しています")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        // フォアグラウンドサービスとして通知を表示
+        startForeground(1, notification)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // GPSの位置情報取得を停止
+        gpsLocationManager.stopLocationUpdates()
+        stopForeground(true) // 通知の削除
+        stopSelf() // サービスの停止
+    }
+
+    override fun onBind(intent: Intent?) = null
+}
